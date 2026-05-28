84a98832495a144f335579e1c2b283356e8b3b3f
Kiaeto tech <kiaetotech@gmail.com>
2026-05-28 22:56:17 +0530

    Minimal App.js - only home and hospital routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ComingSoon from './pages/ComingSoon';
 import HospitalsList from './pages/HospitalsList';
-import HospitalSimpleDetails from './pages/HospitalSimpleDetails';
-import EmergencySearch from './pages/EmergencySearch';
-import BookOPD from './pages/BookOPD';
-import BookAdmission from './pages/BookAdmission';
-import Payment from './pages/Payment';
-import MyBookings from './pages/MyBookings';
 
 function App() {
   return (
@@ -16,20 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/hospitals" element={<HospitalsList />} />
-        <Route path="/hospital-info/:id" element={<HospitalSimpleDetails />} />
-        <Route path="/emergency-search" element={<EmergencySearch />} />
-        <Route path="/book-opd/:id" element={<BookOPD />} />
-        <Route path="/book-admission/:id" element={<BookAdmission />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/ambulance" element={<ComingSoon title="Ambulance Service" />} />
-        <Route path="/insurance" element={<ComingSoon title="Health Insurance" />} />
-        <Route path="/lab-tests" element={<ComingSoon title="Lab Tests" />} />
-        <Route path="/preventive" element={<ComingSoon title="Preventive Checkups" />} />
-        <Route path="/caregivers" element={<ComingSoon title="Caregiver Services" />} />
-        <Route path="/financing" element={<ComingSoon title="Health Financing" />} />
-        <Route path="/teleconsult" element={<ComingSoon title="Teleconsultation" />} />
-        <Route path="/corporate" element={<ComingSoon title="Corporate Health" />} />
       </Routes>
     </BrowserRouter>
   );
