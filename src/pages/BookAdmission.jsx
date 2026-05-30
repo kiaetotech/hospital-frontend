1053cbee97a3814943062cfc8830fcd6146bb6fa
Kiaeto tech <kiaetotech@gmail.com>
2026-05-30 18:48:56 +0530

    Fixed BookAdmission button with console log

--- src/pages/BookAdmission.jsx
+++ src/pages/BookAdmission.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
 const BookAdmission = () => {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1 style={{ color: 'green' }}>BOOK ADMISSION PAGE IS WORKING!</h1>
-      <p>If you see this, the route is working correctly.</p>
-      <a href="/hospitals">Go back to Hospitals</a>
-    </div>
-  );
+  return <h1>Book Admission Page is Working!</h1>;
 };
 
 export default BookAdmission;
