01ef584a9ddf378a3897d708c337672f38894de4
Kiaeto tech <kiaetotech@gmail.com>
2026-05-30 17:56:36 +0530

    Added BookAdmission route and page

--- src/pages/BookAdmission.jsx
+++ src/pages/BookAdmission.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-function BookAdmission() {
-  return <h1>Book Admission Page Working!</h1>;
-}
+const BookAdmission = () => {
+  const { id } = useParams();
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Book Admission Page</h2>
+      <p>Hospital ID: {id}</p>
+      <p>This page is working!</p>
+    </div>
+  );
+};
 
 export default BookAdmission;
