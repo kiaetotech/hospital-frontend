3a5094a6515e06b892ccbbdcef9c10f680fcfb09
Kiaeto tech <kiaetotech@gmail.com>
2026-05-31 14:42:35 +0530

    Added Ambulance route to App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ComingSoon from './pages/ComingSoon';
 import HospitalsList from './pages/HospitalsList';
+import HospitalSimpleDetails from './pages/HospitalSimpleDetails';
+import EmergencySearch from './pages/EmergencySearch';
 import BookOPD from './pages/BookOPD';
 import BookAdmission from './pages/BookAdmission';
 import Payment from './pages/Payment';
 import MyBookings from './pages/MyBookings';
 import Ambulance from './pages/Ambulance';
-import ComingSoon from './pages/ComingSoon';
-import HospitalSimpleDetails from './pages/HospitalSimpleDetails';
-import EmergencySearch from './pages/EmergencySearch';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
+        {/* Main Pages */}
         <Route path="/" element={<HomePage />} />
+        
+        {/* Hospitals Tag */}
         <Route path="/hospitals" element={<HospitalsList />} />
         <Route path="/hospital-info/:id" element={<HospitalSimpleDetails />} />
         <Route path="/emergency-search" element={<EmergencySearch />} />
+        
+        {/* Booking Pages */}
         <Route path="/book-opd/:id" element={<BookOPD />} />
         <Route path="/book-admission/:id" element={<BookAdmission />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/my-bookings" element={<MyBookings />} />
+        
+        {/* Ambulance Tag */}
         <Route path="/ambulance" element={<Ambulance />} />
+        
+        {/* Other Tags (Coming Soon) */}
         <Route path="/insurance" element={<ComingSoon title="Health Insurance" />} />
         <Route path="/lab-tests" element={<ComingSoon title="Lab Tests" />} />
         <Route path="/preventive" element={<ComingSoon title="Preventive Checkups" />} />
