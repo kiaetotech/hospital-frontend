c4333f7c82ac2ff27364c29c62cd632b0d205e87
Kiaeto tech <kiaetotech@gmail.com>
2026-07-13 18:52:41 +0530

    Fixed import path for HomeCollectionSlots

--- src/pages/Diagnostics.jsx
+++ src/pages/Diagnostics.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DiagnosticsCustomPackage from './DiagnosticsCustomPackage';
 import HealthPackagesTab from './HealthPackagesTab';
-import HomeCollectionSlots from '../../components/HomeCollectionSlots';
+import HomeCollectionSlots from '../components/HomeCollectionSlots';
 
 // All 16 Main Categories with their Tests
 const testCategories = [
