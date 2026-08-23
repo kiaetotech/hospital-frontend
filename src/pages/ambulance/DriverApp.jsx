d6df72a751bdc204f77dd5ec734c656f52e6d33a
Kiaeto tech <kiaetotech@gmail.com>
2026-08-23 18:51:24 +0530

    Add api import to DriverApp

--- src/pages/ambulance/DriverApp.jsx
+++ src/pages/ambulance/DriverApp.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
+import api from '../../services/api';
 import {
   getDriverDashboard,
   toggleDriverAvailability,
